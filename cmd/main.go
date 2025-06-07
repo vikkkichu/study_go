058529b86a3abb52571f8567f025ec945d09ae89
Roma <roma.ivanov1999@mail.ru>
2025-06-07 20:16:05 +0300

    add print user

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/vikkkichu/study_go/internal/model"
+	"os"
+)
 
+func main() {
 
+	fileContent, err := os.ReadFile("testdata/user.json")
+	if err != nil {
+		panic(err)
+	}
 
-
+	user := &model.User{}
+	err = json.Unmarshal(fileContent, user)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(user)
+}
